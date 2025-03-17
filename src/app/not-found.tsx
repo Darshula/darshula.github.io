598caf44f5699fbcf1e2e0641ae2a20932ad1348
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-03-17 19:19:14 -0400

    Redirecting all 404s to homepage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,11 +4,11 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Custom404() {
-    const router = useRouter()
+    const router = useRouter();
 
     useEffect(() => {
-        router.replace('/')
-    })
+        router.replace('/');
+    });
 
-    return null
+    return null;
 };
