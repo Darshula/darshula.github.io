bfbcc9f7ac517b56d74839e21d5d90c9e64b9895
Darshula <91152390+Darshula@users.noreply.github.com>
2025-03-19 04:25:27 -0400

    Better sentence structure

--- src/app/views/Attribution.tsx
+++ src/app/views/Attribution.tsx
@@ -2,7 +2,7 @@ export default function Attribution() {
     return (
         <div >
             <p className="!text-slate-500 text-sm tracking-tight">
-                Color scheme and layout inspired from <a className="!text-slate-300" href="https://brittanychiang.com" target="_blank" rel="noreferrer noopener">brittanychiang.com</a>. Built with <span>Next.JS</span> and <span>Tailwind CSS</span>, deployed with <span>GitHub Pages</span>. All text is set in the <span>Inter</span> typeface.
+                Color scheme and layout inspired by <a className="!text-slate-300" href="https://brittanychiang.com" target="_blank" rel="noreferrer noopener">brittanychiang.com</a>. Built with <span>Next.JS</span> and <span>Tailwind CSS</span>, deployed with <span>GitHub Pages</span>. All text is set in the <span>Inter</span> typeface.
             </p>
         </div>
     );
