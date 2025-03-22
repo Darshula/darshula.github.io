6043fc17d885d49da6007398959423cb0869ae89
Darshula <91152390+Darshula@users.noreply.github.com>
2025-03-22 02:33:49 -0400

    Fixed z axis for section headers

--- src/app/views/Navbar.tsx
+++ src/app/views/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 type StickySectionIdentifierProps = { sectionName: string };
 export function StickySectionIdentifier({ sectionName }: StickySectionIdentifierProps) {
     return (
-        <div className="sticky top-0 -mx-6 mb-4 bg-transparent px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <div className="sticky z-100 top-0 -mx-6 mb-4 bg-transparent px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
             <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">{sectionName}</h2>
         </div>
     );
