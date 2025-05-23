286a4d0d04240b6d19d1b6a73ab00115544ace1d
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-05-23 09:54:18 -0400

    Added resume and updated link

--- src/app/views/About.tsx
+++ src/app/views/About.tsx
@@ -1,4 +1,5 @@
 import { StickySectionIdentifier } from "./Navbar";
+import LinkIndicator from "../components/LinkIndicator";
 
 export default function About() {
     return (
@@ -7,7 +8,8 @@ export default function About() {
             <div>
                 <p className="mb-4">Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum (&quot;pain itself&quot;).</p>
                 <p className="mb-4">Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum (&quot;pain itself&quot;).</p>
-                <p className="mb-4">Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum (&quot;pain itself&quot;).</p>
+                <p className="mb-12">Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum (&quot;pain itself&quot;).</p>
+                <p className="font-medium leading-tight"><a href="./TanmayKulkarniResume.pdf" target="_blank" rel="noreferrer noopener" className="group">View Full Résumé<LinkIndicator /></a></p>
             </div>
         </section>
     );
