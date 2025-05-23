a87fa1a507fcb0978b993cf93307ab2dc814bff2
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-05-23 09:54:39 -0400

    Added component for project card

---
+++ src/app/components/DynamicBackground.tsx
@@ -0,0 +1,7 @@
+export default function DynamicBackground() {
+    return (
+        <div
+            className="z-10 s-full inset-0 absolute rounded group-hover:bg-slate-400 opacity-10"
+        />
+    );
+}
