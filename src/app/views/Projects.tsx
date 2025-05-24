e5566f4c4a8966c87373cc772efa9c01176eb6a3
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-05-24 18:37:30 -0400

    Moved projects correctly into separate `li` tags

--- src/app/views/Projects.tsx
+++ src/app/views/Projects.tsx
@@ -7,12 +7,10 @@ export default function Projects() {
         <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
             <StickySectionIdentifier sectionName="Projects" />
             <ul className="group/list">
-                <li className="mb-12">
-                    <ProjectCard name="careerspeak" />
-                    <ProjectCard name="fusionsage" />
-                    <ProjectCard name="webplots" />
-                    <ProjectCard name="neural_network" />
-                </li>
+                <li className="mb-12"><ProjectCard name="careerspeak" /></li>
+                <li className="mb-12"><ProjectCard name="fusionsage" /></li>
+                <li className="mb-12"><ProjectCard name="webplots" /></li>
+                <li className="mb-12"><ProjectCard name="neural_network" /></li>
             </ul>
             <p className="font-medium leading-tight"><a href="https://github.com/Darshula" target="_blank" rel="noreferrer noopener" className="group">View All Projects on Github<LinkIndicator /></a></p>
         </section>
